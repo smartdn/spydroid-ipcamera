19eec287d55117b38fdac7cc6a173c7f21406c9a
duning <duning@hikeen.com.cn>
2019-03-28 14:06:05 +0800

    可编译过,闪退

---
+++ app/src/main/java/net/majorkernelpanic/spydroid/api/CustomRtspServer.java
@@ -0,0 +1,12 @@
+package net.majorkernelpanic.spydroid.api;
+
+import net.majorkernelpanic.streaming.Session;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+public class CustomRtspServer extends RtspServer {
+	public CustomRtspServer() {
+		super();
+		mEnabled = false;
+	}
+}
+
